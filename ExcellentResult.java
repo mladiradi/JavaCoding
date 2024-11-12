3c5a6c7e74c01b68ffd33be37b06f2a9f3989a9e
mladrad <100635431+mladiradi@users.noreply.github.com>
2024-11-12 22:00:44 +0200

    Add files via upload

---
+++ ExcellentResult.java
@@ -0,0 +1,13 @@
+package JavaCoding;
+
+import java.util.Scanner;
+
+public class ExcellentResult {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = Integer.parseInt(scan.nextLine());
+        if (a >= 5) {
+            System.out.println("Excellent!");
+        }
+    }
+}
