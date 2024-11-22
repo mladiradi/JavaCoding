9d093d030dbbd75d5dafc1de2a1fa186ee06fabd
mladrad <100635431+mladiradi@users.noreply.github.com>
2024-11-22 23:17:14 +0200

    Add files via upload

---
+++ FruitOrVegetable.java
@@ -0,0 +1,31 @@
+package JavaCoding;
+
+import java.util.Scanner;
+
+public class FruitOrVegetable {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String name = scan.nextLine();
+        switch (name){
+            case "banana":
+            case "apple":
+            case "kiwi":
+            case "cherry":
+            case "lemon":
+            case "grapes":
+                System.out.println("fruit");
+                break;
+            case "tomato":
+            case "cucumber":
+            case "pepper":
+            case "carrot":
+                System.out.println("vegetable");
+                break;
+            default:
+                System.out.println("unknown");
+                break;
+        }
+    }
+}
+
