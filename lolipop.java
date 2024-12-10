a4a867029d8ad17468221be73ce11e0846e235bb
mladrad <100635431+mladiradi@users.noreply.github.com>
2024-12-10 21:04:29 +0200

    Add files via upload

---
+++ lolipop.java
@@ -0,0 +1,12 @@
+package JavaCoding;
+
+public class lolipop {
+    public static void main(String[] args) {
+
+String s1 = "sfa";
+String s2 = "SFA";
+
+        System.out.println(s1 == s2);
+
+    }
+}
