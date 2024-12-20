a31b1bb333df4e6b091562526cfec7e7b346005d
mladrad <100635431+mladiradi@users.noreply.github.com>
2024-12-20 22:53:11 +0200

    Add files via upload

---
+++ NumbersFrom1To100.java
@@ -0,0 +1,10 @@
+package JavaCoding;
+
+public class NumbersFrom1To100 {
+    public static void main(String[] args) {
+        for (int n = 1; n<=100; n++){
+        System.out.println(n);}
+
+
+    }
+}
