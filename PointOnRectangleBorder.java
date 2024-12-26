a2948328b22c7e011b119e46015eaf085395ef6d
mladrad <100635431+mladiradi@users.noreply.github.com>
2024-12-26 18:55:54 +0200

    Add files via upload

---
+++ PointOnRectangleBorder.java
@@ -0,0 +1,20 @@
+package JavaCoding;
+
+import java.util.Scanner;
+
+public class PointOnRectangleBorder {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int x1 = Integer.parseInt(scan.nextLine());
+        int y1 = Integer.parseInt(scan.nextLine());
+        int x2 = Integer.parseInt(scan.nextLine());
+        int y2 = Integer.parseInt(scan.nextLine());
+        int x = Integer.parseInt(scan.nextLine());
+        int y = Integer.parseInt(scan.nextLine());
+
+       // if (x = x1 || x = x1 && y1 >= y <= y2) {
+
+
+        }
+    }
