5ccf67e653c96bff923631330ef4a75cdaf868cb
mladrad <100635431+mladiradi@users.noreply.github.com>
2025-01-12 20:26:47 +0200

    Add files via upload

---
+++ TrapezArea.java
@@ -0,0 +1,14 @@
+package JavaCoding;
+
+import java.util.Scanner;
+
+public class TrapezArea {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double b1 = Double.parseDouble(scan.nextLine());
+        double b2 = Double.parseDouble(scan.nextLine());
+        double h  = Double.parseDouble(scan.nextLine());
+        double area = (b1 +b2) * h /2;
+        System.out.printf("%.2f",area);
+    }
+}
