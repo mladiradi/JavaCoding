5fe4c3e474b544a805dcdf13549d068ee7692358
mladrad <100635431+mladiradi@users.noreply.github.com>
2025-01-16 22:08:32 +0200

    Add files via upload

---
+++ TriangleArea.java
@@ -0,0 +1,13 @@
+package JavaCoding;
+
+import java.util.Scanner;
+
+public class TriangleArea {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double a = Double.parseDouble(scan.nextLine());
+        double h  = Double.parseDouble(scan.nextLine());
+        double area = a * h /2;
+        System.out.printf("%.2f",area);
+    }
+}
